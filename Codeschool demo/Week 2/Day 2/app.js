83725b5c715689879652362ec4ff73b6715e4d66
nunumarie3000 <96929239+NunuMarie3000@users.noreply.github.com>
2022-06-14 10:02:29 -0500

    added day 2 - HTML tables

---
+++ Codeschool demo/Week 2/Day 2/app.js
@@ -0,0 +1,9 @@
+const students = ['Jordan', 'Curtrick', 'Storm', 'Tyler', 'Linsdey', 'Michael', 'Zion'];
+
+const colors = ['Blue', 'Green', 'Yellow', 'Red', 'Pink', 'Turquoise', 'Orange'];
+
+// row
+// header
+// row
+// data
+
