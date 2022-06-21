0657f17121874ea1cdc78db51c33103a1c89b86e
robertCodedIt <robertarmstrong999999@gmail.com>
2022-06-21 04:51:28 -0500

    adding new week

---
+++ Codeschool demo/Week 3/app.js
@@ -0,0 +1,9 @@
+// This is the very beginning of the Odd Duck Products application. Review the provided code demo as an example.
+
+// This demo should showcase the following features:
+
+// Showcase 2 images, side-by-side that tracks the number of times an image is clicked. After each click, 2 new images are generated.
+// Creating many objects and pushing them into an array
+// Keeping track of when each image is seen
+// keeping track of when each image is clicked
+// Randc
